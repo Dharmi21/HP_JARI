e1d7003b0b1b83627a9951d83b6aac822b1827c1
Dharmi21 <108184565+Dharmi21@users.noreply.github.com>
2023-06-22 19:49:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>HP Jari</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+    <!-- Tweaks for older IEs-->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <!-- owl stylesheets -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+</head>
+
+<body>
+    <!-- header top section start -->
+    <!-- <div class="header_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="call_text"><a href="https://www.google.com/maps/place/793,+New+GIDC,+Old+GIDC,+Katargam,+Surat,+Gujarat+395004/@21.229459,72.841035,15z/data=!4m6!3m5!1s0x3be04edca17f4da7:0x899a6f2fa23e2193!8m2!3d21.2294591!4d72.8410351!16s%2Fg%2F11j24w9n46?hl=en&entry=ttu"><img src="images/map-icon.png"><span class="call_text_left">Locations</span></a></div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="call_text"><a href="#"><img src="images/call-icon.png"><span class="call_text_left">8140021400</span></a></div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="call_text"><a href=""><img src="images/mail-icon.png"><span class="call_text_left"> hp.thread.jari@gmail.com</span></a></div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- header top section end -->
+  <?php
+  include 'navbar.php';
+  ?>
+    <!-- banner section start -->
+    <div class="banner_section layout_padding">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-8 padding_0">
+                    <div class="banner_img"><img src="imgs/banner1.png"></div>
+                </div>
+                <div class="col-sm-4">
+                    <h1 class="clever_text"><span style="color: #000000;">Welcome To The World Of HP Jari</span> 
+                    <div class="read_bt_2"><a href="#">Read More</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- banner section end -->
+    <!-- services section start -->
+    <div class="services_section layout_padding">
+        <div class="container">
+
+            <div class="d-flex  justify-content-center">
+                <h5 class="what_text">What we do</h5>
+            </div>
+            <div class="d-flex  justify-content-center">
+                <h1 class="services_text custom_main">Our Expertize & Services</h1>
+            </div>
+        </div>
+    </div>
+    <!-- services section end -->
+    <!-- services_2 section start -->
+    <div class="services_section_2">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-8">
+                    <div class="plane_main active">
+                        <div class="plane_left">
+                            <div class="icon_1"><img src="images/icon-1.png"></div>
+                        </div>
+                        <div class="plane_right">
+                            <h3 class="vision_text">VISION TO PLAN</h3>
+                            <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
+                        </div>
+                    </div>
+                    <div class="plane_main">
+                        <div class="plane_left">
+                            <div class="icon_1"><img src="images/icon-2.png"></div>
+                        </div>
+                        <div class="plane_right">
+                            <h3 class="vision_text">GROWTH TO VISION</h3>
+                            <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
+                        </div>
+                    </div>
+                    <div class="plane_main">
+                        <div class="plane_left">
+                            <div class="icon_1"><img src="images/icon-3.png"></div>
+                        </div>
+                        <div class="plane_right">
+                            <h3 class="vision_text">MARKET TO GROWTH</h3>
+                            <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
+                        </div>
+                    </div>
+                    <div class="plane_main">
+                        <div class="plane_left">
+                            <div class="icon_1"><img src="images/icon-4.png"></div>
+                        </div>
+                        <div class="plane_right">
+                            <h3 class="vision_text">PLAN TO MARKET</h3>
+                            <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
+                        </div>
+                    </div>
+                    <div class="read_bt"><a href="#">Read More</a></div>
+                </div>
+                <div class="col-sm-12 col-lg-4">
+                    <div class="image_1"><img src="images/img-1.png"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- services_2 section end -->
+    <!-- money section start -->
+    <div class="money_section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 col-lg-4">
+                    <div class="money_taital">
+                        <h3 class="advanced_text">ADVANCED SERVICE</h3>
+                        <h1 class="data_text">Data Analytics</h1>
+                        <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                        <div class="read_bt_1"><a href="#">Read More</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-8 padding_0">
+                    <div class="image_2"><img src="images/img-2.png"></div>
+                    <h1 class="your_text">Your<br> Money<br> Grow</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- money section end -->
+    <!-- client section start -->
+    <div class="client_section layout_padding">
+        <div class="container">
+            <div class="d-flex  justify-content-center">
+                <h1 class="services_text custom_main">Testimonial</h1>
+            </div>
+        </div>
+    </div>
+    <div class="client_section_2 layout_padding">
+        <div class="container">
+            <div id="main_slider" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="client_main"><img src="images/img-3.png"></div>
+                        <div class="client_main_1">
+                            <h1 class="kamlioo_text">kamlioo</h1>
+                            <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                            <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="client_main"><img src="images/img-3.png"></div>
+                        <div class="client_main_1">
+                            <h1 class="kamlioo_text">kamlioo</h1>
+                            <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                            <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="client_main"><img src="images/img-3.png"></div>
+                        <div class="client_main_1">
+                            <h1 class="kamlioo_text">kamlioo</h1>
+                            <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                            <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+                    <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+                    <i class="fa fa-angle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- client section end -->
+    <!-- contact section start -->
+
+        <!-- footer section start -->
+        <?php
+        
+        include 'footer.php'
+        ?>
+      
+    <!-- copyright section end -->
+    <!-- Javascript files-->
+  
+</body>
+
+</html>
